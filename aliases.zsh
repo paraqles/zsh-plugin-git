a1010e7df113ee3047f7d31331678284987242cb
paraqles <paraqles@dthal.net>
2014-08-26 17:19:17 +0200

    Added alias to set global user name "gusrg" and alias for global email "gemlg"

--- aliases.zsh
+++ aliases.zsh
@@ -6,6 +6,8 @@ done
 alias gupgh='git push github'
 
 alias gusr='git config user.name'
+alias gusrg='git config --global user.name'
 alias geml='git config user.email'
+alias gemlg='git config --global user.email'
 
 alias giusr='git config user.name `whoami`'
